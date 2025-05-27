c83f419e37560ccd8f32d0b67615385634f3b91f
Ajay-Raagav17 <ajayraagav02@gmail.com>
2025-05-27 19:55:50 +0530

    Update env.py

--- env.py
+++ env.py
@@ -1,1 +1,2 @@
-bot = BasicBot(api_key, api_secret, testnet=True)
+API_KEY = "iXHwjALPrPayPsfytGEmqkuWYLeiLiEJqUw87GSC44hkwmXUZzwiFM5sT57dQwmd"
+API_SECRET = "XnsRxfIHf7HBZJeoTLsxwmCYns49sOqjHPoo4DQTVsTWp7vpnIS57xblTC5FB6aa"
