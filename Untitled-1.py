d515e034b06a693b31b47a0523f9f89355e50045
Ajay-Raagav17 <ajayraagav02@gmail.com>
2025-06-24 22:26:43 +0530

    Update Untitled-1.py

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,4 +1,4 @@
 # Futures:
 # self.client.futures_create_order(symbol='BTCUSDT', ...)
 # Spot equivalent:
-self.client.create_order(symbol='BTCUSDT', ...)
+self.client.create_order(symbol='BTCUSDT' ...)
